76a619b91ee63cc9b263e5a4dc7e051b14acf601
cirtrus <oliver@hiesl.net>
2016-02-15 19:51:46 +0100

    added Datamodel for Pony

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from 'angular2/core';
+import {Pony} from "./pony";
 @Component({
     selector: 'my-app',
     template: '<h1>My First {{name}} 2 App</h1>'
---
+++ app/pony.ts
@@ -0,0 +1,18 @@
+export class Pony
+{
+  constructor(public firstname:string,
+              public lastname:string,
+              public dateofBirth:number,
+              public gender:number,
+              public height:number,
+              public hobies:Array<any>,
+              public weight:number,
+              public color:string,
+              public loc:Array<number>,
+              public realEstates:Array<any>
+
+            )
+  {
+
+  }
+}
