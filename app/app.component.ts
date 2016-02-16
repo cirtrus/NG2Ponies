2e238578aedecf002bcc38406701cb6f1b578ca7
cirtrus <oliver@hiesl.net>
2016-02-16 22:40:12 +0100

    added getPonies Method on PonyService

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,23 +1,37 @@
-import {Component} from 'angular2/core';
+import {Component,OnInit} from 'angular2/core';
+import {HTTP_PROVIDERS}    from 'angular2/http';
 import {Pony} from "./pony";
 import {PonyDetail} from "./pony-detail.component";
+import {PonyService} from "./pony.service";
 @Component({
     selector: 'my-app',
     directives:[PonyDetail],
-    template: `<pony-detail [pony]="pony"></pony-detail>`
+    providers: [
+  HTTP_PROVIDERS,
+  PonyService
+],
+    template: `<pony-detail [pony]="pony" *ngFor="#pony of ponies"></pony-detail>`
 })
-export class AppComponent {
-    pony:Pony=new Pony("Alex","Fitzgerald",767397600000,0,1.25,
-        [{
-        "name": "Couponing",
-        "started": 1151445600000,
-        "timePerWeek": 5}],49,"#9a1",[-66.38716,-154.68226],[{
-            "worth": 92516,
-            "loc": [
-                40.57906,
-                132.98138
-            ],
-            "type": "Farmhouse"
-        }]);
+export class AppComponent implements OnInit{
+  public ponies:Pony[]=[];
+  public errorMessage;
+  constructor(private _PonyService:PonyService)
+  {
+
+  }
+  getPonies()
+  {
+   this._PonyService.getPonies()
+   .subscribe(
+                  ponies=> {console.log(ponies);this.ponies = ponies},
+                  error =>  this.errorMessage = <any>error);
+                    console.log(this.ponies);
+  }
+
+    ngOnInit()
+    {
+      this.getPonies();
+      console.log(this.ponies);
+    }
 
 }
--- app/pony.service.ts
+++ app/pony.service.ts
@@ -1,5 +1,26 @@
+import {Injectable}     from 'angular2/core';
+import {Http, Response} from 'angular2/http';
+import {Pony}           from './pony';
+import {Observable}     from 'rxjs/Observable';
+@Injectable()
 export class PonyService
 {
+  private ponyUrl="/pony.json";
 
-  
+   constructor(private http:Http)
+   {
+
+   }
+  getPonies()
+  {
+    return this.http.get(this.ponyUrl)
+                    .map(res =><Pony[]>res.json())
+                    .catch(this.handleError);
+  }
+  handleError(error:Response)
+  {
+    console.log(404);
+    return Observable.throw(error.json().error || "404 Error");
+  }
+
 }
